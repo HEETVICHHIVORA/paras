f9658387c0e0500868eac8a0bc9122b2b4a12732
HEETVICHHIVORA <heetvic@gmail.com>
2023-11-18 14:10:17 +0530

    heet

--- heet.cpp
+++ heet.cpp
@@ -1,5 +1,24 @@
-#include<iostream>
-using namespace std;
-int main (){
-    return 0;
-}
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        int  l = nums.size();
+        int s =0 ;
+        int e =l-1;
+        int ans  = INT_MIN;
+        sort(nums.begin(),nums.end());
+        while(s<=e){
+            int sum = nums[s]+nums[e];
+            if(sum>=ans){
+                ans=sum;
+                e--;
+               
+            }
+
+           
+            else{
+            e--;
+            }
+        }
+        return ans;
+    }
+};
